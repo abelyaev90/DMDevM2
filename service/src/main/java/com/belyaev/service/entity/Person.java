fc57848bda3aaadeba8b16a8ed070cd6b95fdb7c
abelyaev90 <tf37a31612@gmail.com>
2023-07-03 17:44:02 +0200

    - создание схемы приложение - добавление основных сущностей - тестов нет(

---
+++ service/src/main/java/com/belyaev/service/entity/Person.java
@@ -0,0 +1,30 @@
+package com.belyaev.service.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class Person {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+    @Column(name = "birth_date")
+    private LocalDate birthDate;
+    private String position;
+    private String image;
+}
